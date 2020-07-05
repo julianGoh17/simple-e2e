893c6e714e3c798a0f5d29a5d7acbc3ca087cc6a
Julian Goh <juliangohsl@gmail.com>
2020-07-05 19:31:05 +0100

    test: Add test automated test step
    Add automated test step which will allow us to
    run unit tests before a unit test.
    
    Contributes to: julianGoh17/simple-e2e#5
    
    Signed-off-by: Julian Goh <juliangohsl@gmail.com>

---
+++ entrypoint.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+set -x 
+set -v 
+
+WORK_DIR="/home/e2e"
+SIMPLE_E2E_PATH="$GOPATH/src/github.com/julianGoh17/simple-e2e/"
+
+echo "Building Simple-E2E binary..."
+cd "$SIMPLE_E2E_PATH/framework" || return
+sudo go build -o ./simple-e2e
+mv ./simple-e2e $WORK_DIR
+cd "$WORK_DIR" || return
+
+# For github action
+if [ "$1" = "unit-test" ]; then 
+    echo "*** Running unit tests***"
+    cd "$SIMPLE_E2E_PATH/framework" || return 
+    sudo go get 
+    sudo go test ./...
+    if [ $? -ne 0 ]; then 
+        exit 1
+    fi
+else 
+    echo "*** Welcome to Simple-E2E container ***"
+    /bin/sh
+fi
